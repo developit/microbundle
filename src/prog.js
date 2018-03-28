240681e05e843845fd00a645d286aac50b683bcb
ngryman <ngryman@gmail.com>
2018-03-29 00:40:06 +0200

    move watch flag up with other aliased flags

--- src/prog.js
+++ src/prog.js
@@ -17,14 +17,14 @@ export default handler => {
 		.option('--entry, -i', 'Entry module(s)')
 		.option('--output, -o', 'Directory to place build files into')
 		.option('--format, -f', 'Only build specified formats', 'es,cjs,umd')
+		.option('--watch, -w', 'Rebuilds on any change', false)
 		.option('--target', 'Specify your target environment', 'node')
 		.option('--external', `Specify external dependencies, or 'all'`)
 		.option('--compress', 'Compress output using UglifyJS', true)
 		.option('--strict', 'Enforce undefined global context and add "use strict"')
 		.option('--name', 'Specify name exposed in UMD builds')
 		.option('--cwd', 'Use an alternative working directory', '.')
-		.option('--sourcemap', 'Generate source map', true)
-		.option('--watch, -w', 'Rebuilds on any change', false);
+		.option('--sourcemap', 'Generate source map', true);
 
 	prog
 		.command('build [...entries]', '', { default: true })
