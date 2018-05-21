dc9d0d0ed6baffd5acd5f1046de5a6bd569f1611
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-05-21 10:25:42 +0200

    Turn off eslint rules inconsistent with prettier settings

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	linters: {
 		'{src,test}/**/*.js': [
 			'prettier --use-tabs --single-quote --trailing-comma=all --write',
+			'eslint --fix',
 			'git add',
 		],
 		'*.md': ['prettier --write', 'git add'],
