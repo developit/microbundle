aa0b8d1d314d173f880409bb10578ca2eefec68d
harmony <me@hrmny.pw>
2018-05-28 16:22:59 +0200

    eslint runs prettier

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,6 @@
 module.exports = {
 	linters: {
-		'{src,test}/**/*.js': [
-			'prettier --use-tabs --single-quote --trailing-comma=all --write',
-			'eslint --fix',
-			'git add',
-		],
+		'{src,test}/**/*.js': ['eslint --fix', 'git add'],
 		'*.md': ['prettier --write', 'git add'],
 	},
 	ignore: ['**/dist/**/*.js'],
