2a92191698240c25fdec6d0ff335431fdb932823
Federico Zivolo <federico.zivolo@gmail.com>
2018-12-10 18:04:15 +0100

    feat: add support for Babel macros

--- src/lib/babel-custom.js
+++ src/lib/babel-custom.js
@@ -77,6 +77,9 @@ export default babelPlugin.custom(babelCore => {
 						name: '@babel/plugin-transform-regenerator',
 						async: false,
 					},
+					{
+						name: 'babel-plugin-macros',
+					},
 				].filter(Boolean),
 			);
 
