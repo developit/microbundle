8c4f5c2f856bf632229790f8b153751875ea9c22
Ward Peeters <ward@coding-tech.com>
2019-05-05 13:20:00 +0200

    refactor(jest): use it.each & remove sourcemaps from snapshots (#386)

--- test/index.test.js
+++ test/index.test.js
@@ -55,64 +55,65 @@ const parseScript = (() => {
 })();
 
 describe('fixtures', () => {
-	fs.readdirSync(FIXTURES_DIR).forEach(fixtureDir => {
-		let fixturePath = resolve(FIXTURES_DIR, fixtureDir);
+	const dirs = fs
+		.readdirSync(FIXTURES_DIR)
+		.filter(fixturePath =>
+			fs.statSync(resolve(FIXTURES_DIR, fixturePath)).isDirectory(),
+		);
 
-		if (!fs.statSync(fixturePath).isDirectory()) {
-			return;
-		}
+	it.each(dirs)(
+		'build %s with microbundle',
+		async fixtureDir => {
+			let fixturePath = resolve(FIXTURES_DIR, fixtureDir);
+			if (fixtureDir.endsWith('-with-cwd')) {
+				fixturePath = resolve(fixturePath, fixtureDir.replace('-with-cwd', ''));
+			}
 
-		it(
-			fixtureDir,
-			async () => {
-				if (fixtureDir.endsWith('-with-cwd')) {
-					fixturePath = resolve(
-						fixturePath,
-						fixtureDir.replace('-with-cwd', ''),
-					);
-				}
+			const dist = resolve(`${fixturePath}/dist`);
+			// clean up
+			await rimraf(dist);
+			await rimraf(resolve(`${fixturePath}/.rts2_cache_cjs`));
+			await rimraf(resolve(`${fixturePath}/.rts2_cache_es`));
+			await rimraf(resolve(`${fixturePath}/.rts2_cache_umd`));
 
-				const dist = resolve(`${fixturePath}/dist`);
-				// clean up
-				await rimraf(dist);
-				await rimraf(resolve(`${fixturePath}/.rts2_cache_cjs`));
-				await rimraf(resolve(`${fixturePath}/.rts2_cache_es`));
-				await rimraf(resolve(`${fixturePath}/.rts2_cache_umd`));
+			const script = await getBuildScript(fixturePath, DEFAULT_SCRIPT);
 
-				const script = await getBuildScript(fixturePath, DEFAULT_SCRIPT);
+			const prevDir = process.cwd();
+			process.chdir(resolve(fixturePath));
 
-				const prevDir = process.cwd();
-				process.chdir(resolve(fixturePath));
+			const parsedOpts = parseScript(script);
 
-				const parsedOpts = parseScript(script);
+			const output = await microbundle({
+				...parsedOpts,
+				cwd: parsedOpts.cwd !== '.' ? parsedOpts.cwd : resolve(fixturePath),
+			});
 
-				const output = await microbundle({
-					...parsedOpts,
-					cwd: parsedOpts.cwd !== '.' ? parsedOpts.cwd : resolve(fixturePath),
-				});
+			process.chdir(prevDir);
 
-				process.chdir(prevDir);
+			const printedDir = printTree([dirTree(fixturePath)]);
 
-				const printedDir = printTree([dirTree(fixturePath)]);
+			expect(
+				[
+					`Used script: ${script}`,
+					'Directory tree:',
+					printedDir,
+					strip(output),
+				].join('\n\n'),
+			).toMatchSnapshot();
 
-				expect(
-					[
-						`Used script: ${script}`,
-						'Directory tree:',
-						printedDir,
-						strip(output),
-					].join('\n\n'),
-				).toMatchSnapshot();
-
-				fs.readdirSync(resolve(dist)).forEach(file => {
+			const files = fs.readdirSync(resolve(dist));
+			expect(files.length).toMatchSnapshot();
+			// we don't realy care about the content of a sourcemap
+			files
+				.filter(file => !/\.map$/.test(file))
+				.forEach(file => {
 					expect(
 						fs.readFileSync(resolve(dist, file)).toString('utf8'),
 					).toMatchSnapshot();
 				});
-			},
-			TEST_TIMEOUT,
-		);
-	});
+		},
+		TEST_TIMEOUT,
+	);
 
 	it('should keep shebang', () => {
 		expect(
