7882e48faf60003a599d55e3ef6eadf1558b5a75
Ward Peeters <ward@coding-tech.com>
2019-05-16 13:10:13 +0200

    upgrade glob pattern

--- sizereport.config.js
+++ sizereport.config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	repo: 'developit/microbundle',
-	path: ['dist/*', 'test/fixtures/**/dist/*'],
+	path: ['dist/!(*.js.map)', 'test/fixtures/**/dist/!(*.map)'],
 	branch: 'master',
 };
