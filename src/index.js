e13ecddc458e6cf9e15604e4dd92be67ee00d17d
Jason Miller <developit@users.noreply.github.com>
2020-04-18 11:27:36 -0400

    lint

--- src/index.js
+++ src/index.js
@@ -529,7 +529,7 @@ function createConfig(options, entry, format, writeMeta) {
 						browser: options.target !== 'node',
 						// defaults + .jsx
 						extensions: ['.mjs', '.js', '.jsx', '.json', '.node'],
-						preferBuiltins: options.target === 'node' ? true : undefined
+						preferBuiltins: options.target === 'node' ? true : undefined,
 					}),
 					commonjs({
 						// use a regex to make sure to include eventual hoisted packages
