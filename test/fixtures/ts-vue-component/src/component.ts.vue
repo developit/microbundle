6677727da15da4f9ee26b6dfc8ca3dee38449ff3
Wladislaw Koschemako <w.koschemako@findologic.com>
2020-12-07 14:28:35 +0100

    add ts vue component test

---
+++ test/fixtures/ts-vue-component/src/component.ts.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="example">{{ message }}</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+			message: 'Hello world!'
+		}
+	}
+}
+</script>
---
+++ test/fixtures/ts-vue-component/src/index.ts
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import hello from './component.ts.vue';
+
+new Vue({
+	el: '#app',
+	components: {
+		hello,
+	},
+});
