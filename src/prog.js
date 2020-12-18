967f8d532785aa7bf8636c5a759759a3e72dcf56
Jason Miller <developit@users.noreply.github.com>
2020-12-18 14:55:00 -0500

    Add --css inline option (#769)
    * Add --css inline option
    
    Fixes #744
    
    * Add CLI option
    
    * Add docs for --css and update --css-modules docs
    
    * Create rude-keys-eat.md
    
    * update snapshot

--- src/prog.js
+++ src/prog.js
@@ -56,6 +56,7 @@ export default handler => {
 		.option('--name', 'Specify name exposed in UMD builds')
 		.option('--cwd', 'Use an alternative working directory', '.')
 		.option('--sourcemap', 'Generate source map')
+		.option('--css', 'Where to output CSS: "inline" or "external"', 'external')
 		.option(
 			'--css-modules',
 			'Turns on css-modules for all .css imports. Passing a string will override the scopeName. eg --css-modules="_[hash]"',
