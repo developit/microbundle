d91cd3b066a21a6caed98cf67e234af6b04f6fbd
Jason Miller <developit@users.noreply.github.com>
2021-10-06 10:03:21 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -487,9 +487,7 @@ function createConfig(options, entry, format, writeMeta) {
 					nodeResolve({
 						mainFields: ['module', 'jsnext', 'main'],
 						browser: options.target !== 'node',
-						exportConditions: [
-							options.target === 'node' ? 'node' : 'browser'
-						],
+						exportConditions: [options.target === 'node' ? 'node' : 'browser'],
 						// defaults + .jsx
 						extensions: ['.mjs', '.js', '.jsx', '.json', '.node'],
 						preferBuiltins: options.target === 'node',
