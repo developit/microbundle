developit/microbundle

commit 2a0ca8843f34c3773bb41eb3f8f571fb6b2b2d52

feat: :sparkles: Closes #497: preserve trailing newline in mangle.json (#882)
Co-authored-by: Leah <github.leah@hrmny.sh>

src/index.js
@@ -30,6 +30,7 @@ import {
 } from './lib/option-normalization';
 import { getConfigFromPkgJson, getName } from './lib/package-info';
 import { shouldCssModules, cssModulesConfig } from './lib/css-modules';
+import { EOL } from 'os';
 
 // Extensions to use when resolving modules
 const EXTENSIONS = ['.ts', '.tsx', '.js', '.jsx', '.es6', '.es', '.mjs'];
@@ -403,9 +404,13 @@ function createConfig(options, entry, format, writeMeta) {
 	const externalTest =
 		external.length === 0 ? id => false : id => externalPredicate.test(id);
 
+	let endsWithNewLine = false;
+
 	function loadNameCache() {
 		try {
-			nameCache = JSON.parse(fs.readFileSync(getNameCachePath(), 'utf8'));
+			const data = fs.readFileSync(getNameCachePath(), 'utf8');
+			endsWithNewLine = data.endsWith(EOL);
+			nameCache = JSON.parse(data);
 			// mangle.json can contain a "minify" field, same format as the pkg.mangle:
 			if (nameCache.minify) {
 				minifyOptions = Object.assign(
@@ -618,7 +623,11 @@ function createConfig(options, entry, format, writeMeta) {
 								if (writeMeta && nameCache) {
 									fs.writeFile(
 										getNameCachePath(),
-										JSON.stringify(nameCache, null, 2),
+										JSON.stringify(
+											endsWithNewLine ? `${nameCache}${EOL}` : nameCache,
+											null,
+											2,
+										),
 										() => {},
 									);
 								}

commit 3b85ded4d3b6c16394c73225ab131eb6a6e05fa3

Fix mangle.json being overwritten with [object Object]
Regression from #882.

src/index.js
@@ -621,15 +621,10 @@ function createConfig(options, entry, format, writeMeta) {
 							// after hook
 							writeBundle() {
 								if (writeMeta && nameCache) {
-									fs.writeFile(
-										getNameCachePath(),
-										JSON.stringify(
-											endsWithNewLine ? `${nameCache}${EOL}` : nameCache,
-											null,
-											2,
-										),
-										() => {},
-									);
+									let filename = getNameCachePath();
+									let json = JSON.stringify(nameCache, null, 2);
+									if (endsWithNewLine) json += EOL;
+									fs.writeFile(filename, json, () => {});
 								}
 							},
 						},