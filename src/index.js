ac6b7330ee69694d32c3287566b23647f175d9ce
Ryan Christian <rchristian@ryanchristian.dev>
2022-05-19 15:18:53 -0500

    fix: Incorrect fallbacks for output filenames

--- src/index.js
+++ src/index.js
@@ -302,9 +302,7 @@ function getMain({ options, entry, format }) {
 	mainsByFormat.es = replaceName(
 		pkg.module && !pkg.module.match(/src\//)
 			? pkg.module
-			: pkg['jsnext:main'] || pkgTypeModule
-			? 'x.esm.js'
-			: 'x.esm.mjs',
+			: pkg['jsnext:main'] || (pkgTypeModule ? 'x.esm.js' : 'x.esm.mjs'),
 		mainNoExtension,
 	);
 
@@ -312,9 +310,7 @@ function getMain({ options, entry, format }) {
 		(pkg.exports && walk(pkg.exports, pkgTypeModule)) ||
 			(pkg.syntax && pkg.syntax.esmodules) ||
 			pkg.esmodule ||
-			pkgTypeModule
-			? 'x.modern.js'
-			: 'x.modern.mjs',
+			(pkgTypeModule ? 'x.modern.js' : 'x.modern.mjs'),
 		mainNoExtension,
 	);
 	mainsByFormat.cjs = replaceName(
